2a5d2f6f6e71e73ded2cd92b54ca13a701ffae75
Nikolinka Cieloszczyk <cnikolinka@gmail.com>
2019-04-15 22:56:22 -0400

    fixed app

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Banner.css";
+
+const Banner = () => (
+  <div className="banner text-center d-flex align-items-center">
+    <p className="m-0">
+      Test your memory! Click on an image to gain a point. Each time you click on a NEW Image you keep scoring. Click on the same image twice and you lose. 
+    </p>
+  </div>
+);
+
+export default Banner;
---
+++ src/images.js
@@ -0,0 +1,29 @@
+import Ross1 from "./images/Ross1.png";
+import Ross2 from "./images/Ross2.png";
+import Ross3 from "./images/Ross3.png";
+import Ross4 from "./images/Ross4.png";
+import Ross5 from "./images/Ross5.png";
+import Ross6 from "./images/Ross6.png";
+import Ross7 from "./images/Ross7.png";
+import Ross8 from "./images/Ross8.png";
+import Ross9 from "./images/Ross9.png";
+import Ross10 from "./images/Ross10.png";
+import Ross11 from "./images/Ross11.png";
+import Ross12 from "./images/Ross12.png";
+
+const images = [
+  Ross1,
+  Ross2,
+  Ross3,
+  Ross4,
+  Ross5,
+  Ross6,
+  Ross7,
+  Ross8,
+  Ross9,
+  Ross10,
+  Ross11,
+  Ross12
+];
+
+export default images;
