4a742738f568f2d8154546dbac9f1fd2b530501d
Nikolinka Cieloszczyk <cnikolinka@gmail.com>
2019-04-16 05:53:56 -0400

    Updates

---
+++ precache-manifest.58a6783946df2f8c75f70ed004ee6240.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "60c0c38addb383a93af7ee124d6996be",
+    "url": "/clicky-game/static/media/Ross5.60c0c38a.png"
+  },
+  {
+    "revision": "dceafad542843a46ab42",
+    "url": "/clicky-game/static/css/main.da2c7b33.chunk.css"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  },
+  {
+    "revision": "015be0247dc3ee2955cc",
+    "url": "/clicky-game/static/js/2.76d8ed81.chunk.js"
+  },
+  {
+    "revision": "f8002372b36f1906d4390b5dcb6a47fc",
+    "url": "/clicky-game/static/media/Ross1.f8002372.png"
+  },
+  {
+    "revision": "a6c61fc3071b3a5911f7d4b32da95ea0",
+    "url": "/clicky-game/static/media/Ross2.a6c61fc3.png"
+  },
+  {
+    "revision": "b55bcb7369c1d24859236657ef7c6d0d",
+    "url": "/clicky-game/static/media/Ross3.b55bcb73.png"
+  },
+  {
+    "revision": "0d324d6e78346ae47202a462e5429848",
+    "url": "/clicky-game/static/media/Ross4.0d324d6e.png"
+  },
+  {
+    "revision": "dceafad542843a46ab42",
+    "url": "/clicky-game/static/js/main.8b972af0.chunk.js"
+  },
+  {
+    "revision": "3ecf46bfef3c924143f5b998d4d5bf05",
+    "url": "/clicky-game/static/media/Ross6.3ecf46bf.png"
+  },
+  {
+    "revision": "4eedcd2882622ef653bc1b02e228a720",
+    "url": "/clicky-game/static/media/Ross7.4eedcd28.png"
+  },
+  {
+    "revision": "8427ec6ea9338f047f19da2df2a9cd7b",
+    "url": "/clicky-game/static/media/Ross8.8427ec6e.png"
+  },
+  {
+    "revision": "946f1d8eb6bcf3ec2564ad8bc1dfe972",
+    "url": "/clicky-game/static/media/Ross9.946f1d8e.png"
+  },
+  {
+    "revision": "f436d1d59f9c4e9d474509fa2b8926dd",
+    "url": "/clicky-game/static/media/Ross10.f436d1d5.png"
+  },
+  {
+    "revision": "0fd523ee9c1bd73ddce0049f7f385c5c",
+    "url": "/clicky-game/static/media/Ross11.0fd523ee.png"
+  },
+  {
+    "revision": "5bb8421e4a4787a8bc6b0b62e36941f2",
+    "url": "/clicky-game/static/media/Ross12.5bb8421e.png"
+  },
+  {
+    "revision": "9e6b0471afec6ae6dc2f5639c9c346a2",
+    "url": "/clicky-game/index.html"
+  }
+];
